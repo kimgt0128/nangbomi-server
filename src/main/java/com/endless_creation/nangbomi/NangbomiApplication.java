d2d16b77391e539d20e8f465b2bac2a68d07b5c5
KKT <kimgt2828@gmail.com>
2024-11-27 17:27:13 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/endless_creation/nangbomi/NangbomiApplication.java
@@ -0,0 +1,13 @@
+package com.endless_creation.nangbomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NangbomiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NangbomiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/endless_creation/nangbomi/NangbomiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endless_creation.nangbomi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NangbomiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
